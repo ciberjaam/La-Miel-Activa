5388b78cb75bf351d6f5bc9056733ee0f5899970
ciberjaam <124893809+ciberjaam@users.noreply.github.com>
2023-06-08 17:24:32 -0400

    actualizacion 08/06/23

--- script.js
+++ script.js
@@ -1,78 +1,85 @@
-document.querySelector("button.button-menu-toggle")
-    .addEventListener("click", function() {
-           document.querySelector(".nav-links").
-                      classList.toggle("nav-links-responsive")})
+document.querySelector("button.button-menu-toggle").addEventListener("click", function() {
+  document.querySelector(".nav-links").classList.toggle("nav-links-responsive");
+});
 
- // Función para cambiar la emisora cuando se selecciona una opción en el <select>
- function cambiarEmisora() {
+// Función para cambiar la emisora cuando se selecciona una opción en el <select>
+function cambiarEmisora() {
   var reproductor = document.getElementById("reproductor-audio");
   var emisoraSeleccionada = document.getElementById("emisoras").value;
+
+  // Crear una nueva etiqueta <source>
   var source = document.createElement("source");
   source.src = emisoraSeleccionada;
   source.type = "audio/mpeg";
-  reproductor.innerHTML = ""; // Eliminar el contenido actual del reproductor
+
+  // Eliminar todas las etiquetas <source> existentes dentro del reproductor
+  while (reproductor.firstChild) {
+    reproductor.removeChild(reproductor.firstChild);
+  }
+
+  // Agregar la nueva etiqueta <source> al reproductor
   reproductor.appendChild(source);
+
+  // Cargar el reproductor
   reproductor.load();
 }
-                      
+
 function calculate() {
-    // Obtener los valores ingresados
-    var dollarValue = document.getElementById("dollarInput").value;
-    var amount = document.getElementById("amountInput").value;
-  
-    // Validar que se hayan ingresado valores válidos
-    if (dollarValue && amount) {
-      // Realizar el cálculo
-      var result = dollarValue * amount;
-  
-      // Mostrar el resultado
-      document.getElementById("result").innerHTML = "Resultado: " + result;
-    } else {
-      // Mostrar mensaje de error si no se ingresaron valores válidos
-      document.getElementById("result").innerHTML = "Por favor, ingresa los valores correctos.";
-    }
+  // Obtener los valores ingresados
+  var dollarValue = document.getElementById("dollarInput").value;
+  var amount = document.getElementById("amountInput").value;
+
+  // Validar que se hayan ingresado valores válidos
+  if (dollarValue && amount) {
+    // Realizar el cálculo
+    var result = dollarValue * amount;
+
+    // Mostrar el resultado
+    document.getElementById("result").innerHTML = "Resultado: " + result;
+  } else {
+    // Mostrar mensaje de error si no se ingresaron valores válidos
+    document.getElementById("result").innerHTML = "Por favor, ingresa los valores correctos.";
   }
-  window.addEventListener("DOMContentLoaded", function() {
-    var carouselContainer = document.getElementById("carousel-container");
-    var carousel = document.getElementById("carousel");
-    var banners = carousel.getElementsByClassName("banner");
-    var currentIndex = 0;
-    var prevBtn = document.getElementById("prevBtn");
-    var nextBtn = document.getElementById("nextBtn");
-    var bannerWidth = banners[0].offsetWidth;
-  
-    // Mostrar el banner actual
-    function showCurrentBanner() {
-      var translateX = -(currentIndex * bannerWidth);
-      carousel.style.transform = `translateX(${translateX}px)`;
-    }
-  
-    // Navegación hacia el banner anterior
-    function showPrevBanner() {
-      currentIndex--;
-      if (currentIndex < 0) {
-        currentIndex = banners.length - 1;
-      }
-      showCurrentBanner();
-    }
-  
-    // Navegación hacia el siguiente banner
-    function showNextBanner() {
-      currentIndex++;
-      if (currentIndex >= banners.length) {
-        currentIndex = 0;
-      }
-      showCurrentBanner();
-    }
-  
-    // Agregar eventos a los botones
-    prevBtn.addEventListener("click", showPrevBanner);
-    nextBtn.addEventListener("click", showNextBanner);
-  
-    // Mostrar el primer banner al cargar la página
-    showCurrentBanner();
-  
-    // Cambiar de banner automáticamente cada 3 segundos
-    setInterval(showNextBanner, 3000);
-  });
+}
+
+var currentSlide = 0;
+var slides = document.getElementsByClassName("slider-item");
+var slideInterval;
+
+function showSlide(n) {
+  for (var i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  currentSlide = (n + slides.length) % slides.length;
+  slides[currentSlide].style.display = "block";
+}
+
+function nextSlide() {
+  showSlide(currentSlide + 1);
+}
+
+function prevSlide() {
+  showSlide(currentSlide - 1);
+}
+
+document.getElementById("prevBtn").addEventListener("click", prevSlide);
+document.getElementById("nextBtn").addEventListener("click", nextSlide);
+
+function stopSlideInterval() {
+  clearInterval(slideInterval);
+}
+
+function startSlideInterval() {
+  slideInterval = setInterval(nextSlide, 3000); // Intervalo de tiempo para el desplazamiento automático (3 segundos en este ejemplo)
+}
+
+var sliderContainer = document.querySelector(".slider-container");
+
+sliderContainer.addEventListener("mouseover", stopSlideInterval);
+sliderContainer.addEventListener("mouseout", startSlideInterval);
+
+// Llama a la función startSlideInterval para comenzar el desplazamiento automático al cargar la página
+startSlideInterval();
+
+
   
